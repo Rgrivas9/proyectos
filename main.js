be832cbd4af4e9b5357a0c3f6790f8a8ef7744de
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-25 23:10:02 +0100

    dayseeker sin terminar

--- main.js
+++ main.js
@@ -6,26 +6,22 @@ const navbar = document.getElementById("navbar");
 
 const hero2 = document.querySelector(".hero2");
 const stick = hero2.offsetTop;
-const navbar2 = document.querySelector(".navbar2")
-
-
+const navbar2 = document.querySelector(".navbar2");
 
 const stickybar = () => {
   if (window.pageYOffset >= sticky) {
     navbar.classList.remove("backnav");
     navbar.classList.add("sticky");
-    
   } else {
     navbar.classList.remove("sticky");
     navbar.classList.add("backnav");
-    
   }
 };
 const stickybar2 = () => {
   if (window.pageYOffset >= stick) {
-    navbar2.classList.add("sticky2")
+    navbar2.classList.add("sticky2");
   } else {
-    navbar2.classList.remove("sticky2")
+    navbar2.classList.remove("sticky2");
   }
 };
 window.addEventListener("scroll", () => stickybar());
